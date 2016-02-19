2183b9b883f56219961bf545fce9ae989ce76121
Amir Nathoo <amir.nathoo@bitfields.io>
2016-02-19 09:17:18 -0800

    Fixed the crash at startup - added didSelectItemAtIndexPath to detect and process taps on a cell

--- reversi/ViewController.swift
+++ reversi/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
-    
-    @IBOutlet weak var boardView: UICollectionView!
 
+    @IBOutlet weak var boardView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       boardView.delegate = self
+        boardView.delegate = self
         boardView.dataSource = self
-    
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,39 +26,25 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     
+    // Returns the number of sections
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        // 1
-        // Return the number of sections
-        return 1
+        return 8
     }
     
+    // Returns the number of items in a section
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 64
+        return 8
     }
     
+    // Populates the collection with cells based on the square cell template
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("square", forIndexPath: indexPath) as! BoardCell
         return cell
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        if let touch = touches.first {
-            
-            let position = touch.locationInView(view)
-            
-            let indexPath = NSIndexPath(forRow: Int(position.x), inSection: Int(position.y))
-            let cell: UICollectionViewCell!
-            print(position)
-            cell = boardView.cellForItemAtIndexPath(indexPath)
-            if(cell != nil)
-            {
-                print(cell.layer.position)
-            }
-            else
-            {
-                print("Out of Bounds!")
-            }
-        }
+    // detects and processes taps in a given cell
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        print(indexPath)
     }
     
     
